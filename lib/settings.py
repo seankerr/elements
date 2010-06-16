7569a9d8d937e72b307ccc0a813a61e99227d6b2
Sean Kerr <sean@optimus.local>
2010-06-16 14:48:15 -0400

    Added example database connectivity settings

--- lib/settings.py
+++ lib/settings.py
@@ -7,19 +7,32 @@
 # Version: $Id$
 
 # ----------------------------------------------------------------------------------------------------------------------
-# DATABASE
+# DATABASE MODEL
 # ----------------------------------------------------------------------------------------------------------------------
 
-# import psycopg2
-#
+#import MySQLdb
+
 #databases = {
 #    "default": {
 #        "host":   "localhost",
 #        "user":   "user",
-#        "passwd": "password",
-#        "db":     "dbname",
-#        "pool":   5,
-#        "api":    psycopg2
+#        "passwd": "pass",
+#        "db":     "mydb",
+#        "pool":   2,
+#        "api":    MySQLdb
+#    }
+#}
+
+#import psycopg2
+
+#databases = {
+#    "default": {
+#        "host":     "localhost",
+#        "user":     "user",
+#        "password": "pass",
+#        "database": "mydb",
+#        "pool":     2,
+#        "api":      psycopg2
 #    }
 #}
 
@@ -33,18 +46,18 @@ model_date_type_err      = "Invalid date"
 model_datetime_type_err  = "Invalid date/time"
 model_domain_type_err    = "Invalid domain"
 model_email_type_err     = "Invalid email address"
-model_float_max_err      = "Maximum size is $3"
-model_float_min_err      = "Minimum size is $3"
+model_float_max_err      = "Maximum size is %3"
+model_float_min_err      = "Minimum size is %3"
 model_float_type_err     = "Invalid decimal value"
-model_int_max_err        = "Maximum size is $3"
-model_int_min_err        = "Minimum size is $3"
+model_int_max_err        = "Maximum size is %3"
+model_int_min_err        = "Minimum size is %3"
 model_int_type_err       = "Invalid numerical value"
 model_ipaddress_type_err = "Invalid IP Address"
-model_money_max_err      = "Maximum size is $3"
-model_money_min_err      = "Minimum size is $3"
+model_money_max_err      = "Maximum size is %3"
+model_money_min_err      = "Minimum size is %3"
 model_money_type_err     = "Invalid money value"
-model_text_max_err       = "Maximum length is $3 characters"
-model_text_min_err       = "Minimum length is $3 characters"
+model_text_max_err       = "Maximum length is %3 characters"
+model_text_min_err       = "Minimum length is %3 characters"
 model_text_regex_err     = "Invalid format"
 model_text_type_err      = "Invalid value"
 model_time_type_err      = "Invalid time"
