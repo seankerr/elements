7e85f0cd1c951ba10f70c788c40726a41071b64a
Sean Kerr <sean@optimus.local>
2010-09-27 12:23:01 -0400

    Added http/__init__.py

---
+++ lib/elements/http/__init__.py
@@ -0,0 +1,6 @@
+# This file is part of Elements.
+# Copyright (c) 2010 Sean Kerr. All rights reserved.
+#
+# The full license is available in the LICENSE file that was distributed with this source code.
+#
+# Author: Sean Kerr <sean@code-box.org>
