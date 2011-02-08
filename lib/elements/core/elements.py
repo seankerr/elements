14247252ccb7fbf2b08c2ba05640514ba39704b7
Sean Kerr <sean@code-box.org>
2011-02-08 11:45:44 -0500

    Added include() function

--- lib/elements/core/elements.py
+++ lib/elements/core/elements.py
@@ -10,3 +10,33 @@ APP_EMAIL   = "sean@code-box.org"
 APP_GIT     = "http://github.com/feti/Elements.git"
 APP_NAME    = "Elements"
 APP_VERSION = "0.1.1-alpha"
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def include (mod_path):
+    """
+    Include a module or a particular attribute of a module.
+
+    @param mod_path (str) The library path to the item that will be dynamically imported.
+    """
+
+    # assume that mod_path is an attribute
+    try:
+        mod_fullpath, mod_attr = mod_path.rsplit(".", 1)
+
+        mod = __import__(mod_fullpath, globals(), locals(), [mod_attr], -1)
+
+        return getattr(mod, mod_attr)
+
+    except:
+        pass
+
+    # perhaps mod_path is a module
+    try:
+        return __import__(mod_path, globals(), locals(), [], -1)
+
+    except:
+        pass
+
+    # can't seem to locate the expected module or attribute
+    raise Exception("Failed to include module or attribute '%s'" % mod_path)
